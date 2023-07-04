3a08a906cfe3f60cd7173038024618e6fe1fbcba
kar-thik-m <sparrowkarthik007@gmail.com>
2023-07-04 07:32:17 +0530

    push

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className='container' >
+      <div className='row'>
+
+        <div className='col-sm-4'>
+          <div className="card1" >
+            <div className="card-body">
+              <h5 class="card-title text-muted text-uppercase text-center">Free</h5>
+              <h6 class="card-price text-center">$0<span class="period">/month</span></h6>
+              <hr />
+              <ul class="fa-ul">
+                <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
+                <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
+                <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
+                <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+                <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited
+                  Private Projects</li>
+                <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated
+                  Phone Support</li>
+                <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain
+                </li>
+                <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
+                  Reports</li>
+              </ul>
+
+              <div class="d-grid">
+                <button type="button" class="btn btn-lg btn-primary" disabled>Button</button>
+              </div>
+            </div>
+          </div>
+          <div />
+        </div>
+
+
+
+        <div className='col-sm-4'>
+          <div className="card2" >
+            <div className="card-body">
+              <h5 class="card-title text-muted text-uppercase text-center">plus</h5>
+              <h6 class="card-price text-center">$9<span class="period">/month</span></h6>
+              <hr />
+              <ul class="fa-ul">
+                <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
+                <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
+                <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
+                <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+                <li class="text"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited
+                  Private Projects</li>
+                <li class="text"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated
+                  Phone Support</li>
+                <li class="text"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain
+                </li>
+                <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
+                  Reports</li>
+              </ul>
+
+              <div class="d-grid">
+                <button type="button" class="btn btn-lg btn-primary" disabled>Button</button>
+              </div>
+            </div>
+          </div>
+          <div />
+        </div>
+
+        <div className='col-sm-4'>
+          <div className="card3" >
+            <div className="card-body">
+              <h5 class="card-title text-muted text-uppercase text-center">pro</h5>
+              <h6 class="card-price text-center">$49<span class="period">/month</span></h6>
+              <hr />
+              <ul class="fa-ul">
+                <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
+                <li><span class="fa-li"><i class="fas fa-check"></i></span>150GB Storage</li>
+                <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
+                <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
+                <li class="text"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited
+                  Private Projects</li>
+                <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated
+                  Phone Support</li>
+                <li class="text"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain
+                </li>
+                <li class="text"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
+                  Reports</li>
+              </ul>
+
+              <div class="d-grid">
+                <button type="button" class="btn btn-lg btn-primary" disabled>Button</button>
+              </div>
+            </div>
+          </div>
+          <div />
+        </div>
+      </div >
+    </div>
+
+
+  );
+}
+
+export default App;
