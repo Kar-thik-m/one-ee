0791fd1c3b70e52fd8e2d9a3495bd53d4ca98c45
kar-thik-m <sparrowkarthik007@gmail.com>
2023-07-17 23:35:15 +0530

    iji

--- src/App.jsx
+++ src/App.jsx
@@ -1,103 +1,266 @@
-
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import './App.css';
+import React,{useState} from 'react';
+
 
 function App() {
+
+   const [count,setCount]=useState(0);
+   const [tfcount,setTfcount]=useState(true);
+   
+
+   const cardcount=()=>{
+    if(tfcount){
+      setTfcount(false);
+       setCount(count+1);
+    }else{
+       setTfcount(true);
+        setCount(count-1);
+    }
+ 
+    
+   };
+       
+      
   return (
-    <div className='container' >
-      <div className='row'>
-
-        <div className='col-sm-4'>
-          <div className="card1" >
-            <div className="card-body">
-              <h5 class="card-title text-muted text-uppercase text-center">Free</h5>
-              <h6 class="card-price text-center">$0<span class="period">/month</span></h6>
-              <hr />
-              <ul class="fa-ul">
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-                <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited
-                  Private Projects</li>
-                <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated
-                  Phone Support</li>
-                <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain
-                </li>
-                <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
-                  Reports</li>
-              </ul>
-
-              <div class="d-grid">
-                <button type="button" class="btn btn-lg btn-primary" disabled>Button</button>
-              </div>
-            </div>
+    
+    <div className="App">
+    
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <a className="navbar-brand" href="#"><b>Start Bootstrap</b></a>
+    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+  
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav mr-auto">
+        <li className="nav-item active">
+          <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+        </li>
+        <li className="nav-item active">
+        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+      </li>
+        <li className="nav-item dropdown">
+          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            Shop
+          </a>
+          <div className="dropdown-menu" aria-labelledby="navbarDropdown">
+            <a className="dropdown-item" href="#">All Products</a>
+            <a className="dropdown-item" href="#">Popular Items</a>
+            <div className="dropdown-divider"></div>
+            <a className="dropdown-item" href="#">New Arrivals</a>
           </div>
-          <div />
-        </div>
+        </li>
+       
+      </ul>
 
-
-
-        <div className='col-sm-4'>
-          <div className="card2" >
-            <div className="card-body">
-              <h5 class="card-title text-muted text-uppercase text-center">plus</h5>
-              <h6 class="card-price text-center">$9<span class="period">/month</span></h6>
-              <hr />
-              <ul class="fa-ul">
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>5GB Storage</li>
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-                <li class="text"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited
-                  Private Projects</li>
-                <li class="text"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated
-                  Phone Support</li>
-                <li class="text"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain
-                </li>
-                <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
-                  Reports</li>
-              </ul>
-
-              <div class="d-grid">
-                <button type="button" class="btn btn-lg btn-primary" disabled>Button</button>
-              </div>
-            </div>
-          </div>
-          <div />
-        </div>
-
-        <div className='col-sm-4'>
-          <div className="card3" >
-            <div className="card-body">
-              <h5 class="card-title text-muted text-uppercase text-center">pro</h5>
-              <h6 class="card-price text-center">$49<span class="period">/month</span></h6>
-              <hr />
-              <ul class="fa-ul">
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>Single User</li>
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>150GB Storage</li>
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>Unlimited Public Projects</li>
-                <li><span class="fa-li"><i class="fas fa-check"></i></span>Community Access</li>
-                <li class="text"><span class="fa-li"><i class="fas fa-times"></i></span>Unlimited
-                  Private Projects</li>
-                <li class="text-muted"><span class="fa-li"><i class="fas fa-times"></i></span>Dedicated
-                  Phone Support</li>
-                <li class="text"><span class="fa-li"><i class="fas fa-times"></i></span>Free Subdomain
-                </li>
-                <li class="text"><span class="fa-li"><i class="fas fa-times"></i></span>Monthly Status
-                  Reports</li>
-              </ul>
-
-              <div class="d-grid">
-                <button type="button" class="btn btn-lg btn-primary" disabled>Button</button>
-              </div>
-            </div>
-          </div>
-          <div />
-        </div>
-      </div >
     </div>
-
-
+    <button className="btn btn-outline-dark" type="submit">
+                            <i className="bi-cart-fill me-1"></i>
+                            Cart
+                            <span className="badge bg-dark text-white ms-1 rounded-pill" >{count}</span> 
+                        </button>
+                        
+  </nav>
+   
+    
+  <header className="bg-dark py-5">
+  <div className="container px-4 px-lg-5 my-5">
+      <div className="text-center text-white">
+          <h1 className="display-4 fw-bolder">Shop in style</h1>
+          <p className="lead fw-normal text-white-50 mb-0">With this shop hompeage template</p>
+      </div>
+  </div>
+</header>
+<section className="py-5">
+<div className="container px-4 px-lg-5 mt-5">
+    <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">
+        <div className="col mb-5">
+            <div className="card h-100">
+                
+                <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                
+                <div className="card-body p-4">
+                    <div className="text-center">
+                        
+                        <h5 className="fw-bolder">Fancy Product</h5>
+                        
+                        $40.00 - $80.00
+                    </div>
+                </div>
+                
+                <div className="card-footer p-4 pt-0 border-top-0 bg-transparent">
+                    <div className="text-center"><a className="btn btn-outline-dark mt-auto" href="#" >View options</a></div>
+                </div>
+            </div>
+        </div>
+        <div className="col mb-5">
+            <div className="card h-100">
+                
+                <div className="badge bg-dark text-white position-absolute" >Sale</div>
+               
+                <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                
+                <div className="card-body p-4">
+                    <div className="text-center">
+                        
+                        <h5 className="fw-bolder">Special Item</h5>
+                        
+                        <div className="d-flex justify-content-center small text-warning mb-2">
+                            <div className="bi-star-fill"></div>
+                            <div className="bi-star-fill"></div>
+                            <div className="bi-star-fill"></div>
+                            <div className="bi-star-fill"></div>
+                            <div className="bi-star-fill"></div>
+                        </div>
+                        
+                        <span className="text-muted text-decoration-line-through">$20.00</span>
+                        $18.00
+                    </div>
+                </div>
+                <button type='button'  onClick={cardcount()}>{tfcount?"add":"remove"}</button>
+               
+            </div>
+        </div>
+        <div className="col mb-5">
+            <div className="card h-100">
+                
+                <div className="badge bg-dark text-white position-absolute" >Sale</div>
+           
+                <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                
+                <div className="card-body p-4">
+                    <div className="text-center">
+                        
+                        <h5 className="fw-bolder">Sale Item</h5>
+                        
+                        <span className="text-muted text-decoration-line-through">$50.00</span>
+                        $25.00
+                    </div>
+                </div>
+                <button type='button'  onClick={cardcount()} >{tfcount?"add":"remove"}</button>
+              
+            </div>
+        </div>
+        <div className="col mb-5">
+            <div className="card h-100">
+         
+                <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                
+                <div className="card-body p-4">
+                    <div className="text-center">
+                        
+                        <h5 className="fw-bolder">Popular Item</h5>
+                        
+                        <div className="d-flex justify-content-center small text-warning mb-2">
+                            <div className="bi-star-fill"></div>
+                            <div className="bi-star-fill"></div>
+                            <div className="bi-star-fill"></div>
+                            <div className="bi-star-fill"></div>
+                            <div className="bi-star-fill"></div>
+                        </div>
+                        
+                        $40.00
+                    </div>
+                </div>
+                <button type='button'  onClick={cardcount} >{tfcount?"add":"remove"}</button>
+                
+            </div>
+        </div>
+        <div className="col mb-5">
+            <div className="card h-100">
+                
+                <div className="badge bg-dark text-white position-absolute" >Sale</div>
+                
+                <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                
+                <div className="card-body p-4">
+                    <div className="text-center">
+                        
+                        <h5 className="fw-bolder">Sale Item</h5>
+                     
+                        <span className="text-muted text-decoration-line-through">$50.00</span>
+                        $25.00
+                    </div>
+                </div>
+                
+                <button type='button'  onClick={cardcount} >{tfcount?"add":"remove"}</button>
+            </div>
+        </div>
+        <div className="col mb-5">
+            <div className="card h-100">
+                
+                <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                
+                <div className="card-body p-4">
+                    <div className="text-center">
+                        
+                        <h5 className="fw-bolder">Fancy Product</h5>
+                      
+                        $120.00 - $280.00
+                    </div>
+                    
+                </div>
+                <button type='button'  onClick={cardcount} >{tfcount?"add":"remove"}</button>
+               
+            </div>
+        </div>
+        <div className="col mb-5">
+            <div className="card h-100">
+               
+                <div className="badge bg-dark text-white position-absolute" >Sale</div>
+                              <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+               
+                <div className="card-body p-4">
+                    <div className="text-center">
+                     
+                        <h5 className="fw-bolder">Special Item</h5>
+                     
+                        <div className="d-flex justify-content-center small text-warning mb-2">
+                            <div className="bi-star-fill"></div>
+                            <div className="bi-star-fill"></div>
+                            <div className="bi-star-fill"></div>
+                            <div className="bi-star-fill"></div>
+                            <div className="bi-star-fill"></div>
+                        </div>
+                        
+                        <span className="text-muted text-decoration-line-through">$20.00</span>
+                        $18.00
+                    </div>
+                    
+                </div>
+                <button type='button'  onClick={cardcount} >{tfcount?"add":"remove"}</button>
+               
+            </div>
+        </div>
+        <div className="col mb-5">
+            <div className="card h-100">
+                
+                <img className="card-img-top" src="https://dummyimage.com/450x300/dee2e6/6c757d.jpg" alt="..." />
+                
+                <div className="card-body p-4">
+                    <div className="text-center">
+                                               <h5 className="fw-bolder">Popular Item</h5>
+                        
+                        <div className="d-flex justify-content-center small text-warning mb-2">
+                            <div className="bi-star-fill"></div>
+                            <div className="bi-star-fill"></div>
+                            <div className="bi-star-fill"></div>
+                            <div className="bi-star-fill"></div>
+                            <div className="bi-star-fill"></div>
+                        </div>
+                                       $40.00
+                    </div>
+                </div>
+                <button type='button'  onClick={cardcount} >{tfcount?"add":"remove"}</button>
+                
+            </div>
+        </div>
+    </div>
+</div>
+</section>
+    </div>
+ 
   );
 }
 
